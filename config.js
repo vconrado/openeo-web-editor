d52c2e15696127593acd8098ebfd71c24cd27729
Vitor Gomes <vconrado@gmail.com>
2022-03-17 12:19:40 +0100

    cleaning config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
 	// Set this to connect to a back-end automatically when opening the Web Editor,
 	// so you could set this to https://example.com and then the Web Editor connects
 	// to the corresponding back-end automatically.
-	serverUrl: 'http://0.0.0.0/openeo-backend/',
+	serverUrl: '',
 
 	// The name of the service
 	serviceName: 'openEO',
